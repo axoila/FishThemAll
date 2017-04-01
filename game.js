a99e9bb8ca5149af318310424d47b5e048b918c6
Ronja <axoila@gmx.de>
2017-04-01 10:57:47 +0000

    implemente mouse follow

--- game.js
+++ game.js
@@ -8,13 +8,18 @@ var overlayCanvas = document.getElementById('canvas_overlay');
 var overlayContext = overlayCanvas.getContext('2d');
 
 var textures = new TextureHandler();
+textures.appendImage("./cursor/cursor.png");
 textures.appendImage("./ship/boot.png");
 textures.appendImage("./fish/fish.png");
 
-
-ticker();
-
+var delta = 0;
+var d = new Date();
+var lastFrame = d.getTime();
 function ticker(){
+  d = new Date();
+  delta = (d.getTime() - lastFrame) / 1000.0;
+  lastFrame = d.getTime();
+  
   waterContext.clearRect(0, 0, waterCanvas.width, waterCanvas.height);
   fishContext.clearRect(0, 0, fishCanvas.width, fishCanvas.height);
   overlayContext.clearRect(0, 0, overlayCanvas.width, overlayCanvas.height);
@@ -25,9 +30,15 @@ function ticker(){
 
 function Player(){
   this.pos = new Vector(210, 210);
-  var speed = 1; //speed in pixels per second
-
+  var speed = 0.001; //speed in pixels per second
+  
   this.draw = function(){
+    var velocity = new Vector(mousePos.x, mousePos.y);
+    velocity.subtract(this.pos).normalize().scalarMultiply(speed);
+    this.pos = (mousePos);
+    
+    //console.log(this.pos);
+    
     textures.drawImage(this.pos, 0, overlayContext, true);
   }
 }
@@ -35,6 +46,37 @@ function Player(){
 function Vector(x, y){
   this.x = x;
   this.y = y;
+  
+  this.subtract = function(vec){
+    this.x -= vec.x;
+    this.y -= vec.y;
+    return this;
+  }
+  
+  this.add = function(vec){
+    this.x += vec.x;
+    this.y += vec.y;
+    return this;
+  }
+  
+  this.scalarMultiply = function(scalar){
+    x *= scalar;
+    y *= scalar;
+    return this;
+  }
+  
+  this.normalize = function(){
+    var length = this.length();
+    if (length > 0){
+      x /= length;
+      y /= length;
+    }
+    return this;
+  }
+  
+  this.length = function(){
+    return Math.sqrt(x*x + y*y);
+  }
 }
 
 function TextureHandler(){
@@ -47,7 +89,7 @@ function TextureHandler(){
       modCanvas.width = this.width;
       modCanvas.height = this.height;
       modCanvas.getContext('2d').drawImage(this, 0, 0, this.width, this.height);
-      modContext = modCanvas.getContext('2d');
+      var modContext = modCanvas.getContext('2d');
       var imgd = modContext.getImageData(0, 0, this.width, this.height);
       var pix = imgd.data;
       for (var i = 0, n = pix.length; i < n; i += 4) {
@@ -75,3 +117,13 @@ function TextureHandler(){
     }
   }
 }
+
+var mousePos = new Vector(0, 0);
+document.onmousemove = function(event){
+  mousePos.x = (event.clientX - canvas_overlay.offsetLeft) * canvas_overlay.width / canvas_overlay.offsetWidth;
+  mousePos.y = (event.clientY - canvas_overlay.offsetTop) * canvas_overlay.height / canvas_overlay.offsetHeight;
+}
+
+
+
+ticker();
