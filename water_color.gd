328ccc9c3032e38145902e710ff64bc8682c3207
Ronja <axoila@gmx.de>
2017-04-01 21:02:34 +0200

    colors work.... without order...

--- water_color.gd
+++ water_color.gd
@@ -1,22 +1,29 @@
 extends Sprite
 
-export var depths = FloatArray()
-export var images = []
+export(String, DIR) var water_directory
 
 onready var player = get_node("../Player")
 
+var dir
+
+var images = []
+
 func _ready():
 	set_process(true)
+	
+	dir = Directory.new()
+	dir.open(water_directory)
+	dir.list_dir_begin()
+	var file_name = dir.get_next()
+	while file_name != "":
+		images.append(load(water_directory + "/" + file_name))
+		file_name = dir.get_next()
+	print(images.size())
 
 func _process(delta):
 	update()
 
 func _draw():
-	var index = -1
-	for i in range(depths.size()):
-		if depths[i] > player.depth:
-			index = i
-			break
-#	var texture1 = ImageTexture()
-#	texture1.create_from_image(images[index])
-#	draw_texture(texture1, Vector2())
+	var index = floor(player.depth / 3) + 1
+	print(index)
+	draw_texture(images[index], Vector2())
