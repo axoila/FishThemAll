74a54a1d52a72734b097edaa20467e3326c5151c
Ronja <axoila@gmx.de>
2017-04-01 21:14:16 +0200

    sucessfully interpolate deepness colors

--- water_color.gd
+++ water_color.gd
@@ -10,20 +10,20 @@ var images = []
 
 func _ready():
 	set_process(true)
-	
-	dir = Directory.new()
-	dir.open(water_directory)
-	dir.list_dir_begin()
-	var file_name = dir.get_next()
-	while file_name != "":
-		images.append(load(water_directory + "/" + file_name))
-		file_name = dir.get_next()
+	for i in range(21):
+		if i < 9:
+			images.append(load("%s/0%s.png" % [water_directory,i+1]))
+		else:
+			images.append(load("%s/%s.png" % [water_directory,i+1]))
+			
 	print(images.size())
 
 func _process(delta):
 	update()
 
 func _draw():
-	var index = floor(player.depth / 3) + 1
-	print(index)
+	var index = fmod(floor(player.depth / 3), 21)
+	var ratio = fmod(player.depth, 3) / 3
+	
 	draw_texture(images[index], Vector2())
+	draw_texture(images[index + 1], Vector2(), Color(1, 1, 1, ratio))
