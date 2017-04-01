e9c70318de54f278a6ad7e63f6f9fa755440c27f
Ronja <axoila@gmx.de>
2017-04-01 19:02:57 +0200

    wonk on water wolors - broken build but who cares

---
+++ game_manager.gd
@@ -0,0 +1,7 @@
+extends Node
+
+func _ready():
+	pass
+
+func hit_fish(fish):
+	get_tree().set_pause(true)
--- water_color.gd
+++ water_color.gd
@@ -6,5 +6,17 @@ export var images = []
 onready var player = get_node("../Player")
 
 func _ready():
-	
-	pass
+	set_process(true)
+
+func _process(delta):
+	update()
+
+func _draw():
+	var index = -1
+	for i in range(depths.size()):
+		if depths[i] > player.depth:
+			index = i
+			break
+#	var texture1 = ImageTexture()
+#	texture1.create_from_image(images[index])
+#	draw_texture(texture1, Vector2())
